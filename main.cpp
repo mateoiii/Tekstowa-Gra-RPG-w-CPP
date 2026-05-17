cda4b8772ae3db59285bfd3b84e6f2e46f540ed4
Mateusz Kufel <mateuszkufel255@gmail.com>
2026-05-17 22:05:59 +0000

    Uproszczono funkcję main, usuwając nieużywany kod i dodając przykładowe wywołania metod klasy Mag.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//main.cpp
+// main.cpp
 
 #include <iostream>
 #include <string>
@@ -6,26 +6,12 @@
 #include "Mag.h"
 #include "Wojownik.h"
 
-
-int main(){
-
-    std::vector < Postac* > gildia;                 // wektor przyjmuje wskaźniki bo gdyby przyjmowało postac to magowi i wojownikowi ucinaloby ich własne metody i stałe
-
-    Mag mojMag("Czarodziej Bożydar");
-    Wojownik mojWoj("Figo");
-
-    gildia.push_back(&mojMag);
-    gildia.push_back(&mojWoj);
-
-    for(Postac* bohater : gildia){                  //pętla zakresowa for, tu zamiast skupiać się JAK coś zrobić (zwykła pętla for) to mówimy CO chcemy zrobić. Składnia for(Typ wymyślona_nazwa : kolekcja). Czytamy normalnie, np dla każdegj postaci (którą nazwe bohater) w gidlii zrób:
-        bohater -> przedstawSie();
-        bohater -> atakuj();
-
-    }
-
-
-
-
+int main() {
+    std::cout << "-----TWORZENIE ORYGINALU-----" << std::endl;
+    Mag oryginalnyMag("Czarodziej Bozydar");
+    oryginalnyMag.podniesPrzedmiot("Magiczna Rozdzka");
+    oryginalnyMag.podniesPrzedmiot("Mikstura Many");
+    oryginalnyMag.pokazPlecak();
 
     return 0;
 }
